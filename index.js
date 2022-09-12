1b290eea4bb44c64f083dc70af40eaaa0498c4ea
Treasure-Porth <112691973+Treasure-Porth@users.noreply.github.com>
2022-09-12 13:57:12 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+let firstNum = document.getElementById("first-num");
+let sign = document.getElementById("sign")
+let result = document.getElementById("result")
+
+function one(){
+    firstNum.textContent += 1
+    console.log(firstNum.textContent)
+}
+function two(){
+    firstNum.textContent += 2
+    console.log(firstNum.textContent)
+}
+function three(){
+    firstNum.textContent += 3
+    console.log(firstNum.textContent)
+}
+function four(){
+    firstNum.textContent += 4
+    console.log(firstNum.textContent)
+}
+function space(){
+    sign.textContent += " "
+    console.log(firstNum.textContent)
+}
+function five(){
+    firstNum.textContent += 5
+    console.log(firstNum.textContent)
+}
+function six(){
+    firstNum.textContent += 6
+    console.log(firstNum.textContent)
+}
+function seven(){
+    firstNum.textContent += 7
+    console.log(firstNum.textContent)
+}
+function eight(){
+    firstNum.textContent += 8
+    console.log(firstNum.textContent)
+}
+function nine(){
+    firstNum.textContent += 9
+    console.log(firstNum.textContent)
+}
+function zero(){
+    firstNum.textContent += 0
+    console.log(firstNum.textContent)
+}
+function plus(){
+    sign.textContent += "+"
+    console.log(firstNum.textContent)
+}
+function minus(){
+    sign.textContent += "-"
+    console.log(firstNum.textContent)
+}
+function gun(){
+    sign.textContent += "x"
+    console.log(firstNum.textContent)
+}
+function divide(){
+    sign.textContent += "/"
+    console.log(firstNum.textContent)
+}
+function equal(){
+    //result.textContent
+    console.log(Write.textContent)
+    console.log(Write.typeof)
+}
+
+console.log(Write.typeof)
