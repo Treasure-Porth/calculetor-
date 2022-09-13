0214a635ef2a1ae882060a8db9495727f61aa703
Treasure-Porth <112691973+Treasure-Porth@users.noreply.github.com>
2022-09-13 14:43:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,177 @@
 let firstNum = document.getElementById("first-num");
 let sign = document.getElementById("sign")
 let result = document.getElementById("result")
+let secondNum = document.getElementById("second-num")
+let lastone = 0
+let lasttwo = 0
+let procria;
+let middle;
+
+firstNum.textContent += "\xa0" 
 
 function one(){
-    firstNum.textContent += 1
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 1 
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 1
+        lastone++;
+    }
 }
 function two(){
-    firstNum.textContent += 2
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 2
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 2
+        lastone++;
+    }
 }
 function three(){
-    firstNum.textContent += 3
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 3
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 3
+        lastone++;
+    }
 }
 function four(){
-    firstNum.textContent += 4
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 4
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 4
+        lastone++;
+    }
 }
 function space(){
-    sign.textContent += " "
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += "\xa0"
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += "\xa0"
+        lastone++;
+    }
 }
 function five(){
-    firstNum.textContent += 5
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 5
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 5
+        lastone++;
+    }
 }
 function six(){
-    firstNum.textContent += 6
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 6
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 6
+        lastone++;    
+    }
 }
 function seven(){
-    firstNum.textContent += 7
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 7
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 7
+        lastone++;
+    }
 }
 function eight(){
-    firstNum.textContent += 8
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 8
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 8
+        lastone++;
+    }
 }
 function nine(){
-    firstNum.textContent += 9
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 9
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 9
+        lastone++;
+    }
 }
 function zero(){
-    firstNum.textContent += 0
-    console.log(firstNum.textContent)
+    if (sign.textContent != "") {
+        secondNum.textContent += 0
+        lasttwo++;
+    }
+    else{
+        firstNum.textContent += 0
+        lastone++;
+    }
 }
 function plus(){
     sign.textContent += "+"
     console.log(firstNum.textContent)
+    middle++
+    procria = "+"
 }
 function minus(){
     sign.textContent += "-"
     console.log(firstNum.textContent)
+    middle++
+    procria = "-"
 }
 function gun(){
     sign.textContent += "x"
     console.log(firstNum.textContent)
+    middle++
+    procria = "*"
 }
 function divide(){
     sign.textContent += "/"
     console.log(firstNum.textContent)
+    middle++
+    procria = "/"
+}
+function equal(){
+    let firstnum = Number(firstNum.textContent)
+    let secondnum = Number(secondNum.textContent)
+    if (procria == "") {
+        result.textContent = " = "
+        result.textContent += firstnum + 0
+
+    }
+    if (procria == "+") {
+        result.textContent = "=" + "\xa0\xa0"
+        result.textContent += firstnum + secondnum
+    }
+    if (procria == "-") {
+        result.textContent = " = "
+        result.textContent += firstnum - secondnum
+    }
+    if (procria == "*") {
+        result.textContent = " = "
+        result.textContent += firstnum * secondnum
+    }
+    if (procria == "/") {
+        result.textContent = " = "
+        result.textContent += firstnum / secondnum
+    }
 }
 
-
-//console.log(Write.typeof)
+function del(){
+    firstNum.textContent = ""
+    secondNum.textContent = ""
+    sign.textContent = ""
+    result.textContent = ""
+}
